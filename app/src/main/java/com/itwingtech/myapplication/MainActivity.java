605dc27f3f9a214daafd04243cc16db0b66c0e9b
shahzaman.surani15@gmail.com <Surani@@15>
2024-09-11 21:52:41 +0500

    Improvements

--- app/src/main/java/com/itwingtech/myapplication/MainActivity.java
+++ app/src/main/java/com/itwingtech/myapplication/MainActivity.java
@@ -1,17 +1,9 @@
 package com.itwingtech.myapplication;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.widget.Toast;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.itwingtech.myawesomeads.MyAwesomeAds;
-import com.itwingtech.myawesomeads.onAdShowed;
 
 public class MainActivity extends AppCompatActivity {
 
